e147fb2b52c696dc60db5ea3d0e56ecbf79200a6
robin-despouys-game-station <robin.despouys@gmail.com>
2021-03-02 22:22:35 +0100

    Add session model, controller and routes to the project

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -3,3 +3,5 @@
 export { AuthController as auth } from "./auth";
 export { ThingController as thing } from "./thing";
 export { UserController as user } from "./user";
+export { SessionController as session } from "./session";
+
