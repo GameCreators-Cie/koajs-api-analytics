970e17daae42b6f01c1e18c767f56480647edcef
robin-despouys-game-station <robin.despouys@gmail.com>
2021-03-02 18:26:46 +0100

    clean up api key validation code

--- src/middlewares/isAuth.ts
+++ src/middlewares/isAuth.ts
@@ -32,40 +32,35 @@ export default async (ctx: Context, next: () => Promise<any>) => {
     let userId: string = "";
 
     const clefs = findApiKeyFromReques(ctx.req.headers, ctx.request.query)
-    console.log(`la clé d'api de la requête : ${clefs}` );
     const lalistedesclesapi = getApiKeys();
-    for(const apikey of lalistedesclesapi){
-      console.log(`une API KEY : ${apikey}` );
-    }
     if (lalistedesclesapi.includes(clefs)){
     	validUser = true;
-	} else {
-
-    
-    jwt.verify(
-      loginToken,
-      process.env.JWT_SECRET,
-      (errorValidation: any, decodedToken: any) => {
-        if (errorValidation == null) {
-          validToken = true;
-          userId = decodedToken.id;
-        } else {
-          ctx.body = errorValidation.message;
-          ctx.status = 401;
+	  } 
+    else {
+      jwt.verify(
+        loginToken,
+        process.env.JWT_SECRET,
+        (errorValidation: any, decodedToken: any) => {
+          if (errorValidation == null) {
+            validToken = true;
+            userId = decodedToken.id;
+          } else {
+            ctx.body = errorValidation.message;
+            ctx.status = 401;
+          }
         }
-      }
-    );
-    if (validToken) {
-      const getUser: any = await UserUtils.getUser(userId);
-      if (getUser.status !== 200) {
-        ctx.body = getUser.body;
-        ctx.status = getUser.status;
-      } else {
-        ctx.request.body.currentUser = getUser.body;
-        validUser = true;
-      }
-      }
-      }
+      );
+      if (validToken) {
+        const getUser: any = await UserUtils.getUser(userId);
+        if (getUser.status !== 200) {
+          ctx.body = getUser.body;
+          ctx.status = getUser.status;
+        } else {
+          ctx.request.body.currentUser = getUser.body;
+          validUser = true;
+        }
+        }
+    }
     if (validUser) {
       return next();
     }
